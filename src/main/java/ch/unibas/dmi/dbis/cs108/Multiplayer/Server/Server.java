4c25534704f06feafa4612744fb07ceee05daae7
Sebastian Lenzlinger <sebastian.lenzlinger@stud.unibas.ch>
2022-03-21 20:44:51 +0100

    What am I doing...

--- src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/Server/Server.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/Server/Server.java
@@ -3,18 +3,22 @@ package ch.unibas.dmi.dbis.cs108.Multiplayer.Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Server {
 
-    int port = 42069;
+    private static final int gamePort = 42069;
+    private static int clientIndex = 0;
+    private HashMap<String, Integer> nameToIndex = new HashMap<>();
+    private HashMap<Integer, ClientHandler> indexToHandler = new HashMap<>();
 
     public static void main(String[] args) {
 
         try {
-            ServerSocket gameServer = new ServerSocket(42069);
+            ServerSocket gameServer = new ServerSocket(gamePort);
             System.out.println("Waiting for a connection on Port 42069");
             while(true) {
-                Socket connector = gameServer.accept();
+                Socket client = gameServer.accept();
             }
         } catch (IOException e) {
             e.printStackTrace();
