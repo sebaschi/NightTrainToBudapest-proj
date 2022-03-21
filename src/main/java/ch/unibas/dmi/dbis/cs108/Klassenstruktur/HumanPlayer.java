dfb2bd37a669e34223d7a416cae170488836a3c7
Jonas <jonas.biedermann@stud.unibas.ch>
2022-03-21 12:47:13 +0100

    finished HumanPlayer class

--- src/main/java/ch/unibas/dmi/dbis/cs108/Klassenstruktur/HumanPlayer.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Klassenstruktur/HumanPlayer.java
@@ -1,4 +1,24 @@
 package ch.unibas.dmi.dbis.cs108.Klassenstruktur;
 
+import java.net.Socket;
+
 public class HumanPlayer extends Human{
+    /**
+     * Creates a new GhostPlayer. Should be used at game start or if a HumanPlayer is turned into a ghost.
+     * @param position position on the train
+     * @param name name. if null, then a default name is used.
+     * @param sock the socket for the player.
+     */
+    public HumanPlayer(int position, String name, Socket sock, boolean isOG) {
+        this.position = position;
+        this.sock = sock;
+        isGhost = false;
+        isPlayer = true;
+        kickedOff = false;
+        if (name == null) {
+            this.name = "Player Nr. " + position;
+        } else this.name = name;
+    }
+
+
 }
