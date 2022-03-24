eaca2e2ff16f9138452c4c584fcdd6742fb38214
Jonas <jonas.biedermann@stud.unibas.ch>
2022-03-24 20:22:40 +0100

    Updated ServerPinger and ClientPinger to terminate if the socket is closed

--- src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/helpers/ClientPinger.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/helpers/ClientPinger.java
@@ -2,6 +2,7 @@ package ch.unibas.dmi.dbis.cs108.Multiplayer.helpers;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Sends a ping to the server ("CPING") every 2 seconds and checks if it has gotten a pingback.
@@ -12,27 +13,28 @@ public class ClientPinger implements Runnable{
     private boolean gotPingBack;    //should be set to true (via setGotPingBack) as soon as the client gets a pingback.
     private boolean isConnected;    //set to true unless the ClientPinger detects a connection loss.
     BufferedWriter out;             //the output of this client through which the pings are sent
-    public boolean isRunning;       //can be set to false to tell the ping-er to quit
+    private Socket socket;
 
     /**
-     *
+     * @param socket the socket the Client is connected to which is used to end the thread if the connection is lost.
      * @param out the output through which the pings are sent.
      */
-    public ClientPinger(BufferedWriter out) {
+    public ClientPinger(BufferedWriter out, Socket socket) {
         gotPingBack = false;
         isConnected = true;
         this.out = out;
-        isRunning = true;
+        this.socket = socket;
     }
 
     @Override
     public void run() {
         try {
-            while (isRunning) {
+            while (socket.isConnected()) {
                 out.write("CPING");
                 Thread.sleep(2000);
                 if (!gotPingBack) isConnected = false;
             }
+            isConnected = false;                 //in case the socket accidentally disconnects (can this happen?)
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
