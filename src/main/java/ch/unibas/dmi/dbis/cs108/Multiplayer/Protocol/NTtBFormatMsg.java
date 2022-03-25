baeaf40304f7bf155d39893ee43a0ba333c91a4d
Sebastian Lenzlinger <sebastian.lenzlinger@stud.unibas.ch>
2022-03-25 14:48:18 +0100

    Added description to NTtBFormatMsg

--- src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/Protocol/NTtBFormatMsg.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Multiplayer/Protocol/NTtBFormatMsg.java
@@ -3,6 +3,15 @@ package ch.unibas.dmi.dbis.cs108.Multiplayer.Protocol;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * This class defines what type the ClientMsgDecoder returns after decoding the message.
+ * This is done so the output can be split into a response to the client and action
+ * in the game logik.
+ * commands should map to methods(maybe classes)
+ * parameters map to method parameters
+ *
+ */
+
 public class NTtBFormatMsg {
 
     private String msgToClient;
