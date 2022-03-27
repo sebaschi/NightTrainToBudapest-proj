e95c804e39062800e3fbd2c857f83cde165aec97
Seraina <seraina.schoeb@unibas.ch>
2022-03-27 23:18:45 +0200

    Documented everything I could and tried unifying format

--- src/main/java/ch/unibas/dmi/dbis/cs108/multiplayer/server/AllClientNames.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/multiplayer/server/AllClientNames.java
@@ -2,7 +2,7 @@ package ch.unibas.dmi.dbis.cs108.multiplayer.server;
 
 /**
  * This class is built to contain the usernames of all players in a single string. This allows a
- * duplicate check (--> ClientHandler) when a new player chooses a name: does the string with all
+ * duplicate check (ClientHandler) when a new player chooses a name: does the string with all
  * the previous names contain the new player's desired username? If yes, he is being assigned a
  * random name. If no, he can keep his desired name.
  */
