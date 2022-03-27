48dffa3fc724467da43fa5c41c54ce9f08619d2d
Jonas <jonas.biedermann@stud.unibas.ch>
2022-03-28 00:15:34 +0200

    applied google style

--- src/main/java/ch/unibas/dmi/dbis/cs108/multiplayer/server/NameGenerator.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/multiplayer/server/NameGenerator.java
@@ -5,27 +5,28 @@ import java.util.Random;
 
 public class NameGenerator {
 
-    /**
-     * Creates a random alteration of a Name by adding 4 numbers at the end of the Name that shall be altered
-     *
-     * @param username the to be altered username
-     * @return username + four numbers
-     */
-    static String randomName(String username) {
-        StringBuilder name;
-        while (true) {
+  /**
+   * Creates a random alteration of a Name by adding 4 numbers at the end of the Name that shall be
+   * altered
+   *
+   * @param username the to be altered username
+   * @return username + four numbers
+   */
+  static String randomName(String username) {
+    StringBuilder name;
+    while (true) {
 
-            name = new StringBuilder();
-            Random r = new Random();
-            for (int i = 0; i < 4; i++) {
-                int c = r.nextInt(10);
-                name.append(c);
-            }
-            if (!AllClientNames.allNames("").contains(username + name)) {
-                break;
-            }
-        }
-        return username + name;
+      name = new StringBuilder();
+      Random r = new Random();
+      for (int i = 0; i < 4; i++) {
+        int c = r.nextInt(10);
+        name.append(c);
+      }
+      if (!AllClientNames.allNames("").contains(username + name)) {
+        break;
+      }
     }
+    return username + name;
+  }
 
 }
