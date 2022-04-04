f4b359caeedb22d7bd42b1be6a3b31556fc49fcb
sebaschi <74497638+sebaschi@users.noreply.github.com>
2022-04-04 12:54:08 +0200

    Added first Documentation comment to CentralServerData

--- src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/CentralServerData.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/CentralServerData.java
@@ -1,7 +1,9 @@
 package ch.unibas.dmi.dbis.cs108.sebaschi;
 
 /**
- * This Class represents
+ * This Class Represents an Object containing different Maps, Lists and Sets wherein a server object
+ * can find all needed data. An instance of this object can also be passed to other class-objects uf
+ * they need the same data.
  */
 public class CentralServerData {
 
