cfe76f266f3a52814d9b4a7a0cd46dbb38163e65
sebaschi <74497638+sebaschi@users.noreply.github.com>
2022-04-08 12:43:36 +0200

    ServerLobby now has a list of open lobbies and a list of all clients on the server.

--- src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/ServerLobby.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/ServerLobby.java
@@ -1,10 +1,16 @@
 package ch.unibas.dmi.dbis.cs108.sebaschi;
 
+import ch.unibas.dmi.dbis.cs108.multiplayer.server.ClientHandler;
+import java.util.List;
+
 /**
  * This class is just everyone on the server, which games are open to join, who is in session. I.E.
  * the context of the game just after joining the server, before starting a game and entering into a
  * lobby.
  */
 public class ServerLobby {
+
+  List<Lobby> openLobbies;
+  List<ClientHandler> allClients;
 
 }
