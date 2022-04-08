e03c14f51fec16233f11355023300e97768da835
sebaschi <74497638+sebaschi@users.noreply.github.com>
2022-04-08 12:08:38 +0200

    Correcions in CentralServerData: wherever a Client object wasa expected has been changed to expect a ClientHandler object. This is because the server sees clients via ClientHandlers but has no access to Client objects.
    Lobby: Field "admin" added which is a ClientHandler type object. This represents who started the game and, for the phase before the gameplay starts, controls relevant functions.

--- src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/Lobby.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/sebaschi/Lobby.java
@@ -1,5 +1,16 @@
 package ch.unibas.dmi.dbis.cs108.sebaschi;
 
+import ch.unibas.dmi.dbis.cs108.multiplayer.server.ClientHandler;
+
+/**
+ * The Lobby one is in after a client sends the CRTGM command. THe Server
+ */
 public class Lobby {
+
+  /**
+   * The Person who created the game and can configure it and decide to start once enough players
+   * have entered the lobby.
+   */
+  ClientHandler admin;
 
 }
