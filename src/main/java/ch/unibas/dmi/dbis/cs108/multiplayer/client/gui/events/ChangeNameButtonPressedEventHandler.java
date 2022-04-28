75f6df8e492787f08d58b35e99051adf208a8e0f
Sebastian Lenzlinger <sebastian.lenzlinger@stud.unibas.ch>
2022-04-28 12:04:08 +0200

    Adding Wrapper Classes for Controlls and corresponding EventHandlers. added different fxml views.

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/multiplayer/client/gui/events/ChangeNameButtonPressedEventHandler.java
@@ -0,0 +1,17 @@
+package ch.unibas.dmi.dbis.cs108.multiplayer.client.gui.events;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ChangeNameButtonPressedEventHandler implements EventHandler<ActionEvent> {
+
+  /**
+   * Invoked when a specific event of the type for which this handler is registered happens.
+   *
+   * @param event the event which occurred
+   */
+  @Override
+  public void handle(ActionEvent event) {
+
+  }
+}
